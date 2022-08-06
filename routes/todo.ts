cd356aa0d7f1dc9b5d1eb6e4fa51163aa19c345c
Jericho Siahaya <jericho.siahaya@gmail.com>
2022-08-06 21:38:02 +0900

    add root route

--- routes/todo.ts
+++ routes/todo.ts
@@ -3,6 +3,9 @@ import todoController from "../controllers/todo.ts";
 
 const router = new Router();
 router
+    .get('/', (context) => {
+        context.response.body = 'The server is alive! 🚀';
+    })
     .get('/todos', todoController.getAll)
     .post("/todos", todoController.createTodo)
     .get("/todos/:id", todoController.getByID)
